4ea3921d8dda3d5b8ed40b00645e9bcf905ac58c
蔡語宸 <genden80@gmail.com>
2025-06-09 03:50:35 +0800

    Modify: Short Base

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/frame-fcu/',  
+  base: '/frame/',  
   plugins: [react()],
 });
 
