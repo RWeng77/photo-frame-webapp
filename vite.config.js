07467ad4311319041c0a2e9054f2c46163c49363
蔡語宸 <genden80@gmail.com>
2025-06-09 03:54:00 +0800

    Modify: Instrction

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/frame/',  
+  base: '/2025-FCU-photo-frame-webapp/',  
   plugins: [react()],
 });
 
